c71f30b224640dc78462a2bf216119db9bd7d237
zhidkovd <84766134+zhidkovd@users.noreply.github.com>
2021-12-09 15:16:04 +0300

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+installeddocker=$(sudo dpkg -l docker-ce)
+if [ "$installeddocker" == ""  ]; then
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install -ydocker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+fi
