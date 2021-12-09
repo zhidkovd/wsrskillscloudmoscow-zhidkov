f4186496079bf475fc9e704483ccf85dca8d0eb6
zhidkovd <84766134+zhidkovd@users.noreply.github.com>
2021-12-09 15:27:02 +0300

    Update up-site.sh

--- up-site.sh
+++ up-site.sh
@@ -5,6 +5,7 @@ sed -i "s/{{ IP_address_project_2 }}/project_2/" /skillcloud-ngsite/balance.conf
 sed -i "s/{{ IP_address_project_3 }}/project_3/" /skillcloud-ngsite/balance.conf
 sed -i "s/image: site/image: site:site/" /skillcloud-ngsite/docker-compose.yml
 sed -i "s/image: balance/image: balance:balance/" /skillcloud-ngsite/docker-compose.yml
-docker build -t site:site -f /skillcloud-ngsite/Dockerfile-site .
-docker build -t balance:balance -f /skillcloud-ngsite/Dockerfile-balance .
-docker-compose up -file /skillcloud-ngsite/docker-compose.yml -d
+cd /skillcloud-ngsite
+docker build -t site:site -f Dockerfile-site .
+docker build -t balance:balance -f Dockerfile-balance .
+docker-compose up -d
