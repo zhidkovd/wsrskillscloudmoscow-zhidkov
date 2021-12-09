ffb4288668e48858b1bb4ece277c71dd907ffc1d
zhidkovd <84766134+zhidkovd@users.noreply.github.com>
2021-12-09 15:16:42 +0300

    Update up-site.sh

--- up-site.sh
+++ up-site.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install -ydocker-ce docker-ce-cli containerd.io
+
+sed -i "s/{{ IP_address_project_1 }}/project_1/" /wsr-skillcloud-moscow/balance.conf
+sed -i "s/{{ IP_address_project_2 }}/project_2/" /wsr-skillcloud-moscow/balance.conf
+sed -i "s/{{ IP_address_project_3 }}/project_3/" /wsr-skillcloud-moscow/balance.conf
+sed -i "s/image: site/image: site:site/" /wsr-skillcloud-moscow/docker-compose.yml
+sed -i "s/image: balance/image: balance:balance/" /wsr-skillcloud-moscow/docker-compose.yml
+docker build -t site:site -f /wsr-skillcloud-moscow/Dockerfile-site .
+docker build -t balance:balance -f /wsr-skillcloud-moscow/Dockerfile-balance .
+docker-compose up -file /wsr-skillcloud-moscow/docker-compose.yml -d
