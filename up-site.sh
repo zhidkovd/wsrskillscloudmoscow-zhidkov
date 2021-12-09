2aa436b3690901a0de02586efb235a0cbf48a281
zhidkovd <84766134+zhidkovd@users.noreply.github.com>
2021-12-09 15:17:49 +0300

    Update up-site.sh

--- up-site.sh
+++ up-site.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed -i "s/{{ IP_address_project_1 }}/project_1/" /wsr-skillcloud-moscow/balance.conf
-sed -i "s/{{ IP_address_project_2 }}/project_2/" /wsr-skillcloud-moscow/balance.conf
-sed -i "s/{{ IP_address_project_3 }}/project_3/" /wsr-skillcloud-moscow/balance.conf
-sed -i "s/image: site/image: site:site/" /wsr-skillcloud-moscow/docker-compose.yml
-sed -i "s/image: balance/image: balance:balance/" /wsr-skillcloud-moscow/docker-compose.yml
-docker build -t site:site -f /wsr-skillcloud-moscow/Dockerfile-site .
-docker build -t balance:balance -f /wsr-skillcloud-moscow/Dockerfile-balance .
-docker-compose up -file /wsr-skillcloud-moscow/docker-compose.yml -d
+sed -i "s/{{ IP_address_project_1 }}/project_1/" /skillcloud-ngsite/balance.conf
+sed -i "s/{{ IP_address_project_2 }}/project_2/" /skillcloud-ngsite/balance.conf
+sed -i "s/{{ IP_address_project_3 }}/project_3/" /skillcloud-ngsite/balance.conf
+sed -i "s/image: site/image: site:site/" /skillcloud-ngsite/docker-compose.yml
+sed -i "s/image: balance/image: balance:balance/" /skillcloud-ngsite/docker-compose.yml
+docker build -t site:site -f /skillcloud-ngsite/Dockerfile-site .
+docker build -t balance:balance -f /skillcloud-ngsite/Dockerfile-balance .
+docker-compose up -file /skillcloud-ngsite/docker-compose.yml -d
